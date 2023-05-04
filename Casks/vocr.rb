3815331534e3578da88053ba905e50a22bb73a60
Jeremiah Ticket <seashellpromises@gmail.com>
2023-05-04 17:37:48 -0600

    Added vocr

---
+++ Casks/vocr.rb
@@ -0,0 +1,11 @@
+cask "vocr" do
+  version "1.0.0-beta.2"
+  sha256 :no_check
+  url "https://github.com/chigkim/VOCR/releases/download/v1.0.0-beta.2/VOCR.v1.0.0-beta.2.zip"
+  name "VOCR"
+  desc "OCR your screen with VO"
+  homepage "https://github.com/chigkim/VOCR"
+
+  app "VOCR.app"
+
+end
